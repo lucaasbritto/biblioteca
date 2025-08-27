d91bf943ba15c8706171c2729051f7b08f59856a
Lucas Britto <mister.britto@hotmail.com>
2025-08-27 19:01:21 -0300

    refactor: Refatorando a tela de Login para usar Quasar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import api from './api'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/global.scss'
+import { Quasar } from 'quasar'
+import quasarIconSet from 'quasar/icon-set/material-icons'
+import 'quasar/src/css/index.sass'
+import '@quasar/extras/material-icons/material-icons.css'
 
 const app = createApp(App)
 
 app.config.globalProperties.$api = api
+
+app.use(Quasar, {
+  plugins: {},
+  iconSet: quasarIconSet,
+})
 
 app.use(createPinia())
 app.use(router)
