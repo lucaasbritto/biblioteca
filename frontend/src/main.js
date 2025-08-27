ca6a7c5f9b6aa8c0bc31af814e1fcd82f303fa5d
Lucas Britto <mister.britto@hotmail.com>
2025-08-27 15:19:23 -0300

    feat: Criação da tela Login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import api from './api'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
+
+app.config.globalProperties.$api = api
 
 app.use(createPinia())
 app.use(router)
